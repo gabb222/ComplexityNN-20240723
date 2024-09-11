0c00745bd493695a3291a53478a80974acd7fdeb
gabb222 <gbruno527@gmail.com>
2024-09-11 15:06:08 -0400

    Change to make compatible with local environment

--- example_metrics.sh
+++ example_metrics.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-python3 metrics.py --processes 2 --directory samples/ --output output
+python metrics.py --processes 2 --directory research_v1/ --output output
 
+python metrics.py --processes 2 --directory VISCHEMA-data/VISC-C/scenes_grey/ --output scene_output_grey
--- example_run.sh
+++ example_run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-python3 run.py --model model/complexity_net.pt --directory samples/ --output output
+python run.py --model model/complexity_net.pt --directory research_v1/ --output output
 
+python run.py --model model/complexity_net.pt --directory VISCHEMA-data/VISC-C/scenes_grey/ --output scene_output_grey
